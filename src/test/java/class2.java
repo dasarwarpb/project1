1a8567a62fc64f152fadf9939eb517fa47c61512
dasar <dasar@LAPTOP-8VCSLMPL.bbrouter>
2025-07-17 23:40:12 +0530

    second commit

---
+++ src/test/java/class2.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class class2 {
+	@Test
+	public void test_method1() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		driver.close();
+	}
+}
